4272924641f78554b29e13ee0b7405ae3d735190
evitadmello21 <evita.dmello@vcti.io>
2024-08-01 15:53:01 +0530

    Added mainMenu method

--- studentRecord.py
+++ studentRecord.py
@@ -22,9 +22,25 @@ class studentScore:
             print("File not found")
             exit()
 
+    def mainMenu(self):
+        """
+        Display the main menu and handle user input.
+        """
+        while True:
+            print("\n-----Main Menu-----")
+            print("1. Retrieve Data")
+            print("2. Exit")
+            choice = int(input("Enter choice: "))
+
+            if choice == 1:
+                rollno = input("Enter student roll number: ")
+                record = self.RetriveStudentScore(rollno)
+                if record:
+                    print(f"Student record for Roll number {rollno}: {record}")
+            elif choice == 2:
+                exit()
+            else:
+                print("Enter correct choice")
 
 f = studentScore('student_data.csv')
-rollno = input("Enter student roll number: ")
-record = f.RetriveStudentScore(rollno)
-if record:
-    print(f"Student record for Roll number {rollno}: {record}")
+f.mainMenu()
