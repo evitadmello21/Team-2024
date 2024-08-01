e6862954b60c7f26786bc797d3d37f0abcce3bb9
evitadmello21 <evita.dmello@vcti.io>
2024-08-01 15:56:53 +0530

    Added StoreStudentMethod method

--- studentRecord.py
+++ studentRecord.py
@@ -22,6 +22,54 @@ class studentScore:
             print("File not found")
             exit()
 
+    def StoreStudentScore(self):
+        """
+        Collect student data from user input and store it in the CSV file.
+        """
+        while True:
+            print("\n1. Save student data")
+            print("2. Go Back")
+            choice = int(input("Enter your choice: "))
+
+            if choice == 1:
+                rollno = input("Enter student roll number: ")
+                name = input("Enter student name: ").capitalize()
+                english = input("Enter English score: ")
+                maths = input("Enter Maths score: ")
+                science = input("Enter Science score: ")
+
+                # Create dictionary to store student record
+                student_record = {
+                    "Rollno": rollno,
+                    "name": name,
+                    "english": english,
+                    "maths": maths,
+                    "science": science
+                }
+
+                # Check missing values
+                missing_value = [key for key, value in student_record.items() if not value]
+
+                if missing_value:
+                    print(f"Failed to store data, following parameters missing: {','.join(missing_value)}")
+
+                else:
+                    try:
+                        with open(self.csv_file, "a", newline="") as file:
+                            field = ['Rollno', 'name', 'english', 'maths', 'science']
+                            write = csv.DictWriter(file, fieldnames=field)
+                            write.writerow(student_record)
+                            print("Record Inserted Successfully.")
+
+                    except Exception as e:
+                        print("File not found")
+                        exit()
+
+            elif choice == 2:
+                self.mainMenu()
+            else:
+                print("Invalid input. Enter correct choice")
+
     def mainMenu(self):
         """
         Display the main menu and handle user input.
@@ -29,7 +77,8 @@ class studentScore:
         while True:
             print("\n-----Main Menu-----")
             print("1. Retrieve Data")
-            print("2. Exit")
+            print("2. Store Student record")
+            print("3. Exit")
             choice = int(input("Enter choice: "))
 
             if choice == 1:
@@ -38,6 +87,8 @@ class studentScore:
                 if record:
                     print(f"Student record for Roll number {rollno}: {record}")
             elif choice == 2:
+                self.StoreStudentScore()
+            elif choice == 3:
                 exit()
             else:
                 print("Enter correct choice")
